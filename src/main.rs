2030b8bec2ad2ef9470b1f2f5a80589d1ef64e32
Sam Willcocks <sam@wlcx.cc>
2020-12-01 16:03:32 +0000

    day 1

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, prelude::*};
+
+fn main() {
+    let fname = env::args().skip(1).next().unwrap();
+    let file = BufReader::new(File::open(fname).unwrap());
+    let input: Vec<i32> = file.lines().map(|val| i32::from_str_radix(&val.unwrap(), 10).unwrap()).collect();
+    for x in 0..input.len() {
+        for y in x+1..input.len() {
+            if input[x] + input[y] == 2020 {
+                println!("part 1: {}", input[x] * input[y]);
+            }
+            for z in y+1..input.len() {
+                // let's be obnoxious
+                if input[x] + input[y] + input[z] == 2020 {
+                    println!("part 2: {}", input[x]*input[y]*input[z])
+                }
+            }
+        }
+    }
+}
